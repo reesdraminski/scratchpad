67d1eda0d62cac134ed63ae6fd75c7872faa834f
Rees Draminski <rees.draminski@gmail.com>
2021-04-15 17:20:39 -0400

    add note tabs

--- js/app.js
+++ js/app.js
@@ -1,28 +1,168 @@
-const notesEl = document.getElementById("notepad"); // notepad DOM reference
 const DELAY = 300; // the input delay time
 const TAB = "&#009";
 
-// attach key listener for shortcut keys
-bindShortcuts();
+const notesEl = document.getElementById("notepad"); // notepad DOM reference
+const navEl = document.getElementById("nav");
+const addNoteTab = document.getElementById("addNoteTab");
 
-// if localStorage is available
-if (typeof(Storage) !== "undefined") {
-    // get the notes from localStorage and put it into notes textarea
-    notesEl.innerHTML = localStorage.getItem("notes");
+const hasLocalStorage = checkForLocalStorage();
+const data = hasLocalStorage ? JSON.parse(localStorage.getItem("notes") || "{}") : {};
+let timeout = 0;
+
+/**
+ * Initialize the UI components on page load.
+ */
+(function initUI() {
+    // attach key listener for shortcut keys
+    bindShortcuts();
+
+    // if there are no notes saved
+    if (!data.notes)
+    {
+        // create a blank note and set it as last active
+        data.notes = [""];
+        data.activeIndex = 0;
+    }
+
+    // render the note tabs and show the current note on the pad
+    renderNotes();
+
+    // bind add note button functionality
+    addNoteTab.onclick = e => {
+        // prevent hash being appended to URL
+        e.preventDefault();
+
+        // add new blank note
+        data.notes.push("");
+
+        // show the newly created note
+        data.activeIndex = data.notes.length - 1;
+
+        // force save of note
+        saveApplicationState();
+
+        // re-render note tabs
+        renderNotes();
+    }
+
+    // autosave notes
+    notesEl.oninput = () => {
+        // if new typing, clear previous delay
+        clearTimeout(timeout);
+
+        // set save to localStorage to constant delay time
+        timeout = setTimeout(() => {
+            // update the note content of the active note
+            data.notes[data.activeIndex] = notesEl.innerHTML;
+
+            // save the updated note state
+            saveApplicationState();
+        }, DELAY);
+    }
+})();
+
+/**
+ * Render the note tabs.
+ */
+function renderNotes() {
+    // show the currently active note
+    notesEl.innerHTML = data.notes[data.activeIndex];
+
+    // delete the note tabs so they can be re-rendered
+    [...nav.children]
+        .filter(x => x.id != "addNoteTab")
+        .forEach(x => x.remove());
+
+    // create tabs for all of the saved notes
+    data.notes
+        .forEach((x, i) => {
+            // create the note tab list item
+            const tab = document.createElement("li");
+            tab.classList.add("nav-item");
+
+            tab.onclick = e => {
+                // prevent hash being appended to URL
+                e.preventDefault();
+
+                // if not clicking on the already active tab
+                if (data.activeIndex != i)
+                {
+                    // set the clicked tab as active
+                    data.activeIndex = i;
+
+                    // save state since the activeIndex changed
+                    saveApplicationState();
+
+                    // re-render the note tabs
+                    renderNotes();
+                }
+            }
+
+            // create the tab title text link element
+            const title = document.createElement("a");
+            title.classList.add("nav-link");
+            title.textContent = `Note #${i + 1}`;
+
+            // show the last active tab as active
+            if (i == data.activeIndex) 
+            {
+                title.classList.add("active");
+            }
+
+            tab.appendChild(title);
+
+            // create the close button
+            const close = document.createElement("button");
+            close.classList.add("btn-close");
+
+            close.onclick = e => {
+                // capture only the close button click and not tab click
+                e.stopPropagation();
+
+                // if the current tab is the last tab, but not the only tab, shift activeIndex left by one
+                if (i == data.activeIndex && i == data.notes.length - 1 && i !== 0)
+                {
+                    data.activeIndex--;
+                }
+
+                // delete the note
+                data.notes.splice(i, 1);
+
+                // if the user deleted the last note, create a new one
+                if (data.notes.length === 0)
+                {
+                    // add a blank note
+                    data.notes.push("");
+                }
+
+                // save state since notes data changed
+                saveApplicationState();
+
+                // re-render note tabs
+                renderNotes();
+            }
+
+            title.appendChild(close);
+
+            // insert the tab into the tab list
+            navEl.insertBefore(tab, addNoteTab);
+        });
 
     // scroll to top of textarea delay 1ms to allow value to be set
     setTimeout(() => notesEl.scrollTop = 0, 1);
-
-    // start autosaving input
-    autosave();
+    notesEl.focus();
 }
 
+/**
+ * Bind shortcut keys.
+ */
 function bindShortcuts() {
     notesEl.onclick = e => {
         // if the user clicked on a link
-        if (e.target.tagName == "A") {
+        if (e.target.tagName == "A") 
+        {
             // open link in new window
-            e.srcElement.target = "_blank";
+            e.target.target = "_blank";
 
             // make the notepad uneditable so the link can get clicked
             notesEl.contentEditable = false;
@@ -33,35 +173,54 @@ function bindShortcuts() {
     }
 
     notesEl.onkeydown = async (e) => {
+        if (e.key == " ")
+        {
+            const lineEl = window.getSelection().focusNode;
+            const text = lineEl.textContent.trim();
+
+            if (text.substring(text.length - 2) == "[]")
+            {
+                const oldHTML = lineEl.parentNode.innerHTML;
+
+                lineEl.parentNode.innerHTML = oldHTML.slice(0, oldHTML.length - 2);
+            }
+        }
+
         // Tab
-        if (e.key == "Tab") {
+        if (e.key == "Tab") 
+        {
             e.preventDefault();
 
             document.execCommand("insertHTML", false, TAB);
         }
         // Ctrl/Cmd + K
-        else if (e.metaKey && e.key === "k") {
+        else if (e.metaKey && e.key === "k") 
+        {
             // get selected text
             let selectedText = getSelectionText();
 
             // read the clipboard
             let clipboardText = await navigator.clipboard.readText();
 
-            // if the user has a valid URL in clipboard and is highlighting regular text
-            if (isValidURL(clipboardText) && !isValidURL(selectedText)) {
-                document.execCommand("createLink", false, clipboardText);
-            }
             // if the user is trying to create a link from a URL in notes
-            else if (isValidURL(selectedText)){
+            if (isValidURL(selectedText))
+            {
                 document.execCommand("createLink", false, selectedText);
+            }
+            // if the user has a valid URL in clipboard and is highlighting regular text
+            else if (isValidURL(clipboardText)) 
+            {
+                document.execCommand("createLink", false, clipboardText);
             }
         }
         // Ctrl/Cmd + U
-        else if (e.metaKey && e.key === "u") {
+        else if (e.metaKey && e.key === "u") 
+        {
             document.execCommand("underline");
         }
         // Ctrl/Cmd + Shift + L
-        else if (e.metaKey && e.shiftKey && e.key === "l") {
+        else if (e.metaKey && e.shiftKey && e.key === "l") 
+        {
             e.preventDefault();
             document.execCommand("insertHTML", false, "<hr/>");
         }
@@ -69,6 +228,7 @@ function bindShortcuts() {
 }
 
 /**
+ * Get the user highlighted text.
  * https://stackoverflow.com/questions/5379120/get-the-highlighted-selected-text
  * @returns {String} selectedText
  */
@@ -86,7 +246,7 @@ function getSelectionText() {
 }
 
 /**
- * 
+ * Check if a string is a valid URL.
  * @param {String} str 
  * @returns {Boolean} isValidURL
  */
@@ -102,16 +262,26 @@ function isValidURL(str) {
 }
 
 /**
- * On textarea input (typing), delay the input by constant delay time to 
- * prevent spamming localStorage.
+ * Save the application state to localStorage.
  */
-let timeout = 0;
-function autosave() {
-    notesEl.oninput = () => {
-        // if new typing, clear previous delay
-        clearTimeout(timeout);
+function saveApplicationState() {
+    localStorage.setItem("notes", JSON.stringify(data));
+}
 
-        // set save to localStorage to constant delay time
-        timeout = setTimeout(() => localStorage.setItem("notes", notesEl.innerHTML), DELAY);
+/**
+ * Check if localStorage exist and is available.
+ * https://stackoverflow.com/questions/16427636/check-if-localstorage-is-available/16427747
+ * @return {Boolean} localStorageExists
+ */
+function checkForLocalStorage(){
+    const test = "test";
+
+    try {
+        localStorage.setItem(test, test);
+        localStorage.removeItem(test);
+
+        return true;
+    } catch(e) {
+        return false;
     }
 }
