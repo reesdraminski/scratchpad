987f0ff8f20aa37f97e264f98706292a9c8d8a9f
Rees Draminski <rees.draminski@gmail.com>
2022-03-28 11:18:51 -0400

    Update isValidURL

--- js/app.js
+++ js/app.js
@@ -267,14 +267,15 @@ function getSelectionText() {
  * @returns {Boolean} isValidURL
  */
 function isValidURL(str) {
-    const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  let url;
+  
+  try {
+    url = new URL(str);
+  } catch (_) {
+    return false;  
+  }
 
-    return !!pattern.test(str);
+  return url.protocol === "http:" || url.protocol === "https:";
 }
 
 /**
